26b118820ca0cdf890d929ff225ddad6c6135597
mitchwong2021 <34347774+mitchwong2021@users.noreply.github.com>
2017-12-24 15:57:18 -0800

    Add files via upload

---
+++ ch06/angryBoss.rb
@@ -0,0 +1,3 @@
+puts 'WHAT DO YOU WANT YOU LOWLY WORKER? YOU USELESS PIECE OF SHIT?'
+request = gets.chomp
+puts 'What do you mean "' + request + '"?!? GET THE FUCK OUT OF MY OFFICE! YOUR\'RE FIRED!!!!!'
---
+++ ch06/tableOfContents.rb
@@ -0,0 +1,10 @@
+line_width = 100
+title = 'The screwed up life of Wilbur Coxis'
+lineOne = 'The time I lost my virginity to a crack whore in Las Vegas'
+lineTwo = 'The time I realized that I was adopted'
+lineThree = 'The time I experienced my first orgy'
+puts (title.center(line_width))
+puts ('1. ' + lineOne.ljust(line_width))
+puts ('2. ' + lineTwo.ljust(line_width))
+puts ('3. ' + lineThree.ljust(line_width))
+
